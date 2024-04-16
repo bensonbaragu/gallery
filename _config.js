3bfedeb5f3c143b9832da4d12de5d9d60e5e3501
bensonbaragu <bensonbaragu@gmail.com>
2024-04-16 13:10:51 +0300

    Updated config.js

--- _config.js
+++ _config.js
@@ -2,8 +2,9 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://bensonbaragu:Ac4XJaH7u3dwyMUA@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://bensonbaragu:Ac4XJaH7u3dwyMUA@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://bensonbaragu:Ac4XJaH7u3dwyMUA@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://bensonbaragu:Ac4XJaH7u3dwyMUA@bbcluster.ryrk4fo.mongodb.net/?retryWrites=true&w=majority&appName=BBCluster',
+    development: 'mongodb+srv://bensonbaragu:Ac4XJaH7u3dwyMUA@bbcluster.ryrk4fo.mongodb.net/?retryWrites=true&w=majority&appName=BBCluster',
+    test: 'mongodb+srv://bensonbaragu:Ac4XJaH7u3dwyMUA@bbcluster.ryrk4fo.mongodb.net/?retryWrites=true&w=majority&appName=BBCluster',
 }
 module.exports = config;
+
